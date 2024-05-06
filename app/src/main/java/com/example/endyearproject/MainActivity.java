1599f7e790ece840b3e78fd99fd3f6ac58a45e96
Magshimim <ac8274@bs.amalnet.k12.il>
2024-05-06 12:45:49 +0300

    static intents class added(expirimental)

--- app/src/main/java/com/example/endyearproject/MainActivity.java
+++ app/src/main/java/com/example/endyearproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.endyearproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -7,10 +8,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity {
+    boolean isFirstTime = false;
+    boolean adding;
+    @Override
+    protected void onStart() {
+        super.onStart();
+        isFirstTime = true;
+        adding = true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(!isFirstTime)
+        {
+            Intent gi = new Intent();
+            Intent ig = getIntent();
+        }
     }
 }
---
+++ app/src/main/java/com/example/endyearproject/activitiesIntents.java
@@ -0,0 +1,10 @@
+package com.example.endyearproject;
+
+import android.content.Intent;
+
+public class activitiesIntents {
+    public static Intent mainIntent;
+    public static Intent allUsersIntent;
+    public static Intent filtersIntent;
+    public static Intent creditsIntent;
+}
